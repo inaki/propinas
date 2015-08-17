d2ebb635cd3c53a338da84bb0a8d0f39105e961c
Imanol Aranzadi <iiaranzadi@gmail.com>
2015-08-16 23:55:51 -0700

    delted uikit segmented control and added a new custom one, prettier!

--- propinas/ViewController.swift
+++ propinas/ViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate, SettingsViewControllerDelegate{
+class ViewController: UIViewController,UITextFieldDelegate, SettingsViewControllerDelegate, SMSegmentViewDelegate{
     
     @IBOutlet weak var billAmountField: UITextField!
     @IBOutlet weak var tipOutputLabel: UILabel!
     @IBOutlet weak var totalOutputLabel: UILabel!
     @IBOutlet weak var tipPctTabs: UISegmentedControl!
+    
+    var segmentView: SMSegmentView!
+    var margin: CGFloat = 10.0
     
     var poor = Double()
     var average = Double()
@@ -23,14 +26,77 @@ class ViewController: UIViewController,UITextFieldDelegate, SettingsViewControll
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor(white: 0.95, alpha: 1.0)
+        
         poor = 0.1
         average = 0.15
         good = 0.20
         self.billAmountField.becomeFirstResponder()
         
+        /*
+        Init SMsegmentView
+        Use a Dictionary here to set its properties.
+        Each property has its own default value, so you only need to specify for those you are interested.
+        */
+        self.segmentView = SMSegmentView(frame: CGRect(x: self.margin, y: 250.0, width: self.view.frame.size.width - self.margin*2, height: 40.0), separatorColour: UIColor(white: 0.95, alpha: 0.3), separatorWidth: 0.5, segmentProperties: [keySegmentTitleFont: UIFont.systemFontOfSize(12.0), keySegmentOnSelectionColour: UIColor(red: 245.0/255.0, green: 174.0/255.0, blue: 63.0/255.0, alpha: 1.0), keySegmentOffSelectionColour: UIColor.whiteColor(), keyContentVerticalMargin: Float(10.0)])
+        
+        self.segmentView.delegate = self
+        
+        self.segmentView.layer.cornerRadius = 5.0
+        self.segmentView.layer.borderColor = UIColor(white: 0.85, alpha: 1.0).CGColor
+        self.segmentView.layer.borderWidth = 1.0
+        
+        // Add segments
+        self.segmentView.addSegmentWithTitle("poor", onSelectionImage: UIImage(named: "poor_light"), offSelectionImage: UIImage(named: "poor"))
+        self.segmentView.addSegmentWithTitle("average", onSelectionImage: UIImage(named: "average_light"), offSelectionImage: UIImage(named: "average"))
+        self.segmentView.addSegmentWithTitle("good", onSelectionImage: UIImage(named: "good_light"), offSelectionImage: UIImage(named: "good"))
+        
+        // Set segment with index 0 as selected by default
+        //segmentView.selectSegmentAtIndex(0)
+        
+        self.view.addSubview(self.segmentView)
+        
     }
     
+    // SMSegment Delegate
+    func segmentView(segmentView: SMSegmentView, didSelectSegmentAtIndex index: Int) {
+        let tipPercentages = [poor, average, good]
+        let tipPercentage = tipPercentages[index]
+        
+        let moneyFormatString = "$%.2f"
+        
+        let billAmount = (billAmountField.text as NSString).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipOutputLabel.text = String(format: moneyFormatString, tip)
+        totalOutputLabel.text = String(format: moneyFormatString, total)
+        println("Select segment at index: \(index)")
+        
+        
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.All.rawValue)
+    }
+    
+    override func willAnimateRotationToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
+        /*
+        MARK: Replace the following line to your own frame setting for segmentView.
+        */
+        if toInterfaceOrientation == UIInterfaceOrientation.LandscapeLeft || toInterfaceOrientation == UIInterfaceOrientation.LandscapeRight {
+            self.segmentView.organiseMode = .SegmentOrganiseVertical
+            self.segmentView.segmentVerticalMargin = 25.0
+            self.segmentView.frame = CGRect(x: self.view.frame.size.width/2 - 40.0, y: 100.0, width: 80.0, height: 220.0)
+        }
+        else {
+            self.segmentView.organiseMode = .SegmentOrganiseHorizontal
+            self.segmentView.segmentVerticalMargin = 10.0
+            self.segmentView.frame = CGRect(x: self.margin, y: 120.0, width: self.view.frame.size.width - self.margin*2, height: 40.0)
+        }
+    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -42,17 +108,7 @@ class ViewController: UIViewController,UITextFieldDelegate, SettingsViewControll
     }
     
     func updateTip() {
-        let tipPercentages = [poor, average, good]
-        let tipPercentage = tipPercentages[tipPctTabs.selectedSegmentIndex]
         
-        let moneyFormatString = "$%.2f"
-        
-        let billAmount = (billAmountField.text as NSString).doubleValue
-        let tip = billAmount * tipPercentage
-        let total = billAmount + tip
-        
-        tipOutputLabel.text = String(format: moneyFormatString, tip)
-        totalOutputLabel.text = String(format: moneyFormatString, total)
     }
     
     @IBAction func onTap(sender: AnyObject) {
@@ -61,19 +117,7 @@ class ViewController: UIViewController,UITextFieldDelegate, SettingsViewControll
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
-        let defaults = NSUserDefaults.standardUserDefaults()
-        var defaultTip = defaults.integerForKey("default_tip")
-        println("got default tip: \(defaultTip)")
-        switch defaultTip {
-        case 0...2:
-            tipPctTabs.selectedSegmentIndex = defaultTip
-        default:
-            tipPctTabs.selectedSegmentIndex = 0
-        }
-        
-        // Recalculate the tip
-        updateTip()
+
     }
     
     func doubleToTextNoDecimal(tip : Double) -> String {
